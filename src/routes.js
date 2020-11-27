e2482cde71a60d68343f7936462d6681907ebbdf
Guilherme Matos de Menezes <guiimenezes.ti@gmail.com>
2020-11-27 16:13:52 -0300

    criação da autenticação de token no lado do servidor

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,7 @@ routes.get('/api/usuarios/details/:_id', Usuario.details)
 routes.delete('/api/usuarios/:_id', Usuario.delete)
 routes.put('/api/usuarios/', Usuario.update)
 routes.post('/api/usuarios/login', Usuario.login)
+routes.post('/api/usuarios/checkToken', Usuario.checkToken)
 
 // Rotas de produtos
 routes.post('/api/produtos', Produto.create)
